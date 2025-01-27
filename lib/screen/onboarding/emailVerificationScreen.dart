6e1591581902774206840cf89c2af03fd02111b4
nihadcodes <nihad.exe@gmail.com>
2025-01-27 12:54:39 +0600

    done with onboarding functions

--- lib/screen/onboarding/emailVerificationScreen.dart
+++ lib/screen/onboarding/emailVerificationScreen.dart
@@ -1,15 +1,40 @@
 import 'package:flutter/material.dart';
 
+import '../../api/apiClient.dart';
 import '../../style/style.dart';
-
 class emailVerificationScreen extends StatefulWidget {
-  const emailVerificationScreen({super.key});
-
+  const emailVerificationScreen({Key? key}) : super(key: key);
   @override
   State<emailVerificationScreen> createState() => _emailVerificationScreenState();
 }
 
 class _emailVerificationScreenState extends State<emailVerificationScreen> {
+
+  Map<String,String> FormValues={"email":""};
+  bool Loading=false;
+
+  InputOnChange(MapKey, Textvalue){
+    setState(() {
+      FormValues.update(MapKey, (value) => Textvalue);
+    });
+  }
+
+  FormOnSubmit() async{
+    if(FormValues['email']!.length==0){
+      ErrorToast('Email Required !');
+    }
+    else{
+      setState((){Loading=true;});
+      bool res=await VerifyEmailRequest(FormValues['email']);
+      if(res==true){
+        Navigator.pushNamed(context, "/pinVerification");
+      }
+      else{
+        setState(() {Loading=false;});
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,30 +42,36 @@ class _emailVerificationScreenState extends State<emailVerificationScreen> {
         children: [
           ScreenBackground(context),
           Container(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-
-              children: [
-                Text("Your Email Address", style: Head1Text(colorDarkBlue)),
-                SizedBox(height: 1),
-                Text("A 6 digit verification pin will send to your email address", style: Head6Text(colorLightGray)),
-                SizedBox(height: 20),
-                TextFormField(decoration: AppInputDecoration("Email Address"),),
-
-                SizedBox(height: 20),
-                Container(
-                  child: ElevatedButton(
-                    style: AppButtonStyle(),
-                    child: SuccessButtonChild('Next'),
-                    onPressed: () {
-
-                    },
-                  ),
-                )
-
-              ],
-            ),
+              alignment: Alignment.center,
+              child: Loading?(Center(child: CircularProgressIndicator())):(
+                  SingleChildScrollView(
+                    padding: EdgeInsets.all(30),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Your Email Address", style: Head1Text(colorDarkBlue)),
+                        SizedBox(height: 1),
+                        Text("A 6 digit verification pin will send to your email address", style: Head6Text(colorLightGray)),
+                        SizedBox(height: 20),
+                        TextFormField(
+                          onChanged: (Textvalue){
+                            InputOnChange("email",Textvalue);
+                          },
+                          decoration: AppInputDecoration("Email Address"),
+                        ),
+                        SizedBox(height: 20),
+                        Container(child: ElevatedButton(
+                          style: AppButtonStyle(),
+                          child: SuccessButtonChild('Next'),
+                          onPressed: (){
+                            FormOnSubmit();
+                          },
+                        ),)
+                      ],
+                    ),
+                  )
+              )
           )
         ],
       ),
